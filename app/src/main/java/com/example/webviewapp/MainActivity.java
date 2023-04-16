aa73cac573ba6445373666cc62fa6586e221ae95
Lina H <f22linhe@student.his.se>
2023-04-16 12:13:48 +0200

    Skapade en variabel av klassen WebView med namnet myWebView. För detta importerades även en klass av typen WebView.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
